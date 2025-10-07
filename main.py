842f4fd3f2b67ec1fa0e9299f8e0e37ddc0a3435
Brenda-Machado <araucaria2011@gmail.com>
2025-10-07 15:15:31 -0300

    somo more stat functions, print formated, sys path and args

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ How to build yout first bookbot
 main.py
 
 """
-
-from stats import word_count, simbols_count
+import sys
+from stats import word_count, simbols_count, sorted_list_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -15,18 +15,39 @@ def get_book_text(filepath):
     
     return file_contents
 
-
 def main():
-    content = get_book_text("books/frankenstein.txt")
+
+    print("============ BOOKBOT ============")
+
+    filepath = sys.argv
+
+    if len(filepath) == 2:
+        
+        content = get_book_text(filepath[1])
+
+        print(f"Analyzing book found at {filepath[1]}")
+
+        print("----------- Word Count ----------")
+
+        num_words = word_count(content)
+
+        print(f"Found {num_words} total words")
+
+        sim_count = simbols_count(content)
+
+        print("--------- Character Count -------")
+
+        report = sorted_list_dict(sim_count)
+
+        for item in report:
+            if item['name'].isalpha():
+
+                print(f"{item['name']}: {item['num']}")
     
-    # print(content)
+    else:
 
-    num_words = word_count(content)
+        print("Usage: python3 main.py <path_to_book>")
 
-    print(f"Found {num_words} total words")
-
-    sim_count = simbols_count(content)
-
-    print(sim_count)
+        sys.exit(1)
 
 main()
--- stats.py
+++ stats.py
@@ -25,3 +25,17 @@ def simbols_count(book):
             simbols[simbol] += 1
     
     return simbols
+
+def sort_on(items):
+    return items["num"]
+
+def sorted_list_dict(simbols):
+    list_simbols = []
+
+    for simbol in simbols:
+
+        list_simbols.append({"name": simbol, "num": simbols[simbol]})
+
+    list_simbols.sort(reverse=True, key=sort_on)
+
+    return list_simbols
